1338d36559cdf0d8a2a254284cd028c29a99504c
Jose Ramón García <jrgarcia@paycomet.com>
2021-10-25 15:28:11 +0200

    Se permite poner en on-hold los pedidos

--- Model/Apm/Giropay/PaymentMethod.php
+++ Model/Apm/Giropay/PaymentMethod.php
@@ -299,26 +299,9 @@ class PaymentMethod extends \Magento\Payment\Model\Method\AbstractMethod impleme
         return $this;
     }
 
-    /**
-     * Accept under review payment.
-     *
-     * @param \Magento\Payment\Model\InfoInterface $payment
-     *
-     * @return $this
-     *
-     * @throws \Magento\Framework\Exception\LocalizedException
-     */
-    public function acceptPayment(\Magento\Payment\Model\InfoInterface $payment)
-    {
-        throw new \Magento\Framework\Exception\LocalizedException(__('Acept Paaym'));
-        return $this;
-    }
+    public function acceptPayment(\Magento\Payment\Model\InfoInterface $payment){}
 
-    public function hold(\Magento\Payment\Model\InfoInterface $payment)
-    {
-        throw new \Magento\Framework\Exception\LocalizedException(__('Hold Paaym'));
-        return $this;
-    }
+    public function hold(\Magento\Payment\Model\InfoInterface $payment){}
 
 
     /**
