6e37499e9ac9e5f671fcc243e7a87e459af1379e
Jose Ramón García <jrgarcia@paycomet.com>
2022-06-14 08:27:17 +0200

    Información APM Multibanco

--- Block/Checkout/Onepage/Success.php
+++ Block/Checkout/Onepage/Success.php
@@ -4,54 +4,90 @@ namespace Paycomet\Payment\Block\Checkout\Onepage;
 
 class Success extends \Magento\Framework\View\Element\Template
 {
-    /**
-     * @var \Magento\Checkout\Model\Session
-     */
-    private $_checkoutSession;
+	/**
+	 * @var \Magento\Checkout\Model\Session
+	 */
+	private $_checkoutSession;
 
-    /**
-     * @var \Magento\Customer\Model\Session
-     */
-    private $_customerSession;
+	/**
+	 * @var \Magento\Customer\Model\Session
+	 */
+	private $_customerSession;
 
-    /**
-     * @param \Magento\Framework\View\Element\Template\Context $context
-     * @param \Magento\Checkout\Model\Session                  $checkoutSession
-     * @param \Magento\Customer\Model\Session                  $customerSession
-     * @param array                                            $data
+	 /**
+     * @var \Magento\Framework\UrlInterface
      */
-    public function __construct(
-        \Magento\Framework\View\Element\Template\Context $context,
-        \Magento\Checkout\Model\Session $checkoutSession,
-        \Magento\Customer\Model\Session $customerSession,
-        array $data = []
-    ) {
-        $this->_checkoutSession = $checkoutSession;
-        $this->_customerSession = $customerSession;
-        parent::__construct($context, $data);
-    }
+    protected $_urlBuilder;
 
-    /**
-     * Return success message if paycomet payment.
-     *
-     * @return string
-     */
-    protected function _toHtml()
-    {
-        $order = $this->_checkoutSession->getLastRealOrder();
-        if (!$order) {
-            return '';
-        }
-        if ($order->getId()) {
-            if ($order->getPayment()->getMethodInstance()->getCode() == 'paycomet_payment') {
-                $this->addData(
-                    [
-                    'is_paycomet' => true,
-                    ]
-                );
-                return parent::_toHtml();
-            }
-        }
-        return '';
-    }
+	/**
+	 * @param \Magento\Framework\View\Element\Template\Context $context
+	 * @param \Magento\Checkout\Model\Session                  $checkoutSession
+	 * @param \Magento\Customer\Model\Session                  $customerSession
+	 * @param array                                            $data
+	 */
+	public function __construct(
+		\Magento\Framework\View\Element\Template\Context $context,
+		\Magento\Checkout\Model\Session $checkoutSession,
+		\Magento\Customer\Model\Session $customerSession,
+		array $data = []
+	) {
+		$this->_urlBuilder = $context->getUrlBuilder();
+		$this->_checkoutSession = $checkoutSession;
+		$this->_customerSession = $customerSession;
+		parent::__construct($context, $data);
+	}
+
+	/**
+	 * Return success message if paycomet payment.
+	 *
+	 * @return string
+	 */
+	protected function _toHtml()
+	{
+
+		$order = $this->_checkoutSession->getLastRealOrder();
+		if (!$order) {
+			return '';
+		}
+		if ($order->getId()) {
+			if ($order->getPayment()->getMethodInstance()->getCode() == 'paycomet_payment') {
+				$this->addData(
+					[
+					'is_paycomet' => true,
+					]
+				);
+				return parent::_toHtml();
+			}
+			if ($order->getPayment()->getMethodInstance()->getCode() == 'paycomet_multibanco') {
+				$MethodData = $order->getPayment()->getAdditionalInformation('METHOD_DATA');
+
+				if ($MethodData) {
+					$methodData = json_decode($MethodData);
+
+					$code = $order->getPayment()->getMethodInstance()->getCode();
+					$title = $order->getPayment()->getMethodInstance()->getTitle();
+					$this->addData(
+						[
+						'code' => $code,
+						'title' => $title,
+						'img_src' => $this->getViewFileUrl('Paycomet_Payment::img/apms/'.$code . '.svg')
+						]
+					);
+
+					if ($methodData->entityNumber && $methodData->referenceNumber) {
+						$this->addData(
+							[
+							'Entity' => $methodData->entityNumber,
+							'Reference' => $methodData->referenceNumber
+							]
+						);
+					}
+
+				}
+
+				return parent::_toHtml();
+			}
+		}
+		return '';
+	}
 }
